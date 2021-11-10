7093d200d4b198a76159da5224e7bcf8f7900e1d
Eckord <eckord@outlook.com>
2021-11-09 19:44:21 -0600

    modificacion solicitud

--- resources/views/servicios/solicitud.blade.php
+++ resources/views/servicios/solicitud.blade.php
@@ -266,7 +266,7 @@ select.list-dt:focus {
     <div class="row justify-content-center mt-0">
         <div class="col-11 col-sm-9 col-md-7 col-lg-6 text-center p-0 mt-3 mb-2">
             <div class="card px-0 pt-4 pb-0 mt-3 mb-3">
-                <h2><strong>Aprobar solicitud</strong></h2>
+                <h2><strong>Solicitud</strong></h2>
                 <div class="row">
                     <div class="col-md-12 mx-0">
                         <form id="msform" method="POST" action="">
